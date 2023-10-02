eef609f40d11f448224695dd55178ff97e117e6f
Lyst67 <p_listopad@ukr.net>
2023-10-02 13:05:55 +0300

    update

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,12 +1,12 @@
 import { Link, useLocation } from "react-router-dom"
-import css from '../../pages/Home/Home.module.css'
+import css from './Movielist.module.css'
 
 const MoviesList = ({ movies }) => {
     const location = useLocation()
     return (
         <ul>
             {movies.map(({ id, title }) => {
-                return <li className={css.list_item} key={id}><Link to={`/movies/${id}`} state={{ from: location }}>{title}</Link></li>
+                return <li className={css.list_item} key={id}><Link className={css.list_link} to={`/movies/${id}`} state={{ from: location }}>{title}</Link></li>
             })}
                     
         </ul>
