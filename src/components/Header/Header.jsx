918cdc4f2325befd0118d204e457cc9d5d5ac39a
Lyst67 <p_listopad@ukr.net>
2024-03-06 18:57:32 +0200

    apd-favicon-name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import css from './Header.module.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import css from './Header.module.css';
 
 const Header = () => {
   return (
     <div className={css.header}>
       <nav className={css.header_nav}>
-        <NavLink className={css.header_link} to="/">Home</NavLink>
-        <NavLink className={css.header_link} to="/movies">Movies</NavLink>
+        <NavLink className={css.header_link} to="/">
+          Home
+        </NavLink>
+        <NavLink className={css.header_link} to="/movies">
+          Movies
+        </NavLink>
       </nav>
+      <h1 className={css.title}>BestMovies</h1>
     </div>
-  )
+  );
 };
 export default Header;
